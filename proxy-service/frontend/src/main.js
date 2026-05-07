845dbe153f0cd6a6e0af215c63fd732f7814cff3
ogsupra_notebook <rilsi222005@gmail.com>
2026-05-07 03:29:33 +0300

    main.js добавлен (заглушки)

---
+++ proxy-service/frontend/src/main.js
@@ -0,0 +1,40 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
+
+// Компоненты страниц (простые заглушки, потом заменить на полноценные)
+import Register from './views/Register.vue'
+import Login from './views/Login.vue'
+import Profile from './views/Profile.vue'
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/register', component: Register },
+  { path: '/login', component: Login },
+  { path: '/profile', component: Profile }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
